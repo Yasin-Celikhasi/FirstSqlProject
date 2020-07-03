f57440e982dff3d272eddc8918a7939d8fdf206d
Yasin-Celikhasi <64683028+Yasin-Celikhasi@users.noreply.github.com>
2020-07-03 15:52:57 +0300

    first commit

---
+++ Personel/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Personel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.personelDataSet = new Personel.PersonelDataSet();
+            this.kisilerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kisilerTableAdapter = new Personel.PersonelDataSetTableAdapters.KisilerTableAdapter();
+            this.personelNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelSoyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelGörevDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelMaasDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelSehirDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kisilerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.personelNoDataGridViewTextBoxColumn,
+            this.personelAdDataGridViewTextBoxColumn,
+            this.personelSoyadDataGridViewTextBoxColumn,
+            this.personelGörevDataGridViewTextBoxColumn,
+            this.personelMaasDataGridViewTextBoxColumn,
+            this.personelSehirDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.kisilerBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(652, 446);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // personelDataSet
+            // 
+            this.personelDataSet.DataSetName = "PersonelDataSet";
+            this.personelDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kisilerBindingSource
+            // 
+            this.kisilerBindingSource.DataMember = "Kisiler";
+            this.kisilerBindingSource.DataSource = this.personelDataSet;
+            // 
+            // kisilerTableAdapter
+            // 
+            this.kisilerTableAdapter.ClearBeforeFill = true;
+            // 
+            // personelNoDataGridViewTextBoxColumn
+            // 
+            this.personelNoDataGridViewTextBoxColumn.DataPropertyName = "PersonelNo";
+            this.personelNoDataGridViewTextBoxColumn.HeaderText = "PersonelNo";
+            this.personelNoDataGridViewTextBoxColumn.Name = "personelNoDataGridViewTextBoxColumn";
+            // 
+            // personelAdDataGridViewTextBoxColumn
+            // 
+            this.personelAdDataGridViewTextBoxColumn.DataPropertyName = "PersonelAd";
+            this.personelAdDataGridViewTextBoxColumn.HeaderText = "PersonelAd";
+            this.personelAdDataGridViewTextBoxColumn.Name = "personelAdDataGridViewTextBoxColumn";
+            // 
+            // personelSoyadDataGridViewTextBoxColumn
+            // 
+            this.personelSoyadDataGridViewTextBoxColumn.DataPropertyName = "PersonelSoyad";
+            this.personelSoyadDataGridViewTextBoxColumn.HeaderText = "PersonelSoyad";
+            this.personelSoyadDataGridViewTextBoxColumn.Name = "personelSoyadDataGridViewTextBoxColumn";
+            // 
+            // personelGörevDataGridViewTextBoxColumn
+            // 
+            this.personelGörevDataGridViewTextBoxColumn.DataPropertyName = "PersonelGörev";
+            this.personelGörevDataGridViewTextBoxColumn.HeaderText = "PersonelGörev";
+            this.personelGörevDataGridViewTextBoxColumn.Name = "personelGörevDataGridViewTextBoxColumn";
+            // 
+            // personelMaasDataGridViewTextBoxColumn
+            // 
+            this.personelMaasDataGridViewTextBoxColumn.DataPropertyName = "PersonelMaas";
+            this.personelMaasDataGridViewTextBoxColumn.HeaderText = "PersonelMaas";
+            this.personelMaasDataGridViewTextBoxColumn.Name = "personelMaasDataGridViewTextBoxColumn";
+            // 
+            // personelSehirDataGridViewTextBoxColumn
+            // 
+            this.personelSehirDataGridViewTextBoxColumn.DataPropertyName = "PersonelSehir";
+            this.personelSehirDataGridViewTextBoxColumn.HeaderText = "PersonelSehir";
+            this.personelSehirDataGridViewTextBoxColumn.Name = "personelSehirDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 446);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kisilerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PersonelDataSet personelDataSet;
+        private System.Windows.Forms.BindingSource kisilerBindingSource;
+        private PersonelDataSetTableAdapters.KisilerTableAdapter kisilerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelSoyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelGörevDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelMaasDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelSehirDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Personel/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Personel
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Personel;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Kisiler", connection);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand kayitekle = new SqlCommand("insert into Kisiler (PersonelNo,PersonelAd,PersonelSoyad,PersonelGörev,PersonelMaas,PersonelSehir) values (@p1,@p2,@p3,@p4,@p5,@p6)", connection);
+            kayitekle.Parameters.AddWithValue("@p1", textBox1.Text);
+            kayitekle.Parameters.AddWithValue("@p2", textBox2.Text);
+            kayitekle.Parameters.AddWithValue("@p3", textBox3.Text);
+            kayitekle.Parameters.AddWithValue("@p4", textBox6.Text);
+            kayitekle.Parameters.AddWithValue("@p5", textBox5.Text);
+            kayitekle.Parameters.AddWithValue("@p6", textBox6.Text);
+
+            kayitekle.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Kayıt Eklendi!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand delete = new SqlCommand("Delete from Kisiler where PersonelAd=@adi", connection);
+            delete.Parameters.AddWithValue("@adi", textBox2.Text);
+            delete.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Kayıt Silindi!");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            string no = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            string ad = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            string soyad = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            string gorev = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            string maas = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            string sehir = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+
+            textBox1.Text = no;
+            textBox2.Text = ad;
+            textBox3.Text = soyad;
+            textBox4.Text = gorev;
+            textBox5.Text = maas;
+            textBox6.Text = sehir;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            SqlCommand update = new SqlCommand("update Kisiler set PersonelNo=@p1,PersonelAd=@p2,PersonelSoyad=@p3,PersonelGörev=@p4,PersonelMaas=@p5,PersonelSehir=@p6 where PersonelNo=@p1", connection);
+            update.Parameters.AddWithValue("@p1", textBox1.Text);
+            update.Parameters.AddWithValue("@p2", textBox2.Text);
+            update.Parameters.AddWithValue("@p3", textBox3.Text);
+            update.Parameters.AddWithValue("@p4", textBox4.Text);
+            update.Parameters.AddWithValue("@p5", textBox5.Text);
+            update.Parameters.AddWithValue("@p6", textBox6.Text);
+
+            update.ExecuteNonQuery();
+
+            connection.Close();
+            MessageBox.Show("Kayıt Güncellendi!");
+        }
+    }
+}
